25085fefdb6de34692c912968b50211854f5e6e6
Wichert Akkerman <wichert@wiggy.net>
2008-02-20 22:05:38 +0000

    plone is a namespace
    svn path=/plone.app.protect/trunk/; revision=19420

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name='plone.app.protect',
       url='http://svn.plone.org/svn/plone/plone.app.protect',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone.app'],
+      namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
