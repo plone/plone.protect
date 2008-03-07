e0ee808c118c194e2f5de693dd955e9d05747f20
Wichert Akkerman <wichert@wiggy.net>
2008-03-07 22:27:41 +0000

    Test the PostOnly checker
    svn path=/plone.app.protect/trunk/; revision=19700

---
+++ plone/app/protect/tests/testPostOnly.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest import TestSuite
+from unittest import makeSuite
+from plone.app.protect.postonly import check
+from ZPublisher.HTTPRequest import HTTPRequest
+from zExceptions import Forbidden
+
+
+class PostOnlyTests(TestCase):
+    def makeRequest(self, method):
+        return HTTPRequest(None,
+                           dict(REQUEST_METHOD=method,
+                                SERVER_PORT="80",
+                                SERVER_NAME="localhost"),
+                           None)
+
+    def testNonHTTPRequestAllowed(self):
+        check("not a request")
+
+    def testGETRequestForbidden(self):
+        self.assertRaises(Forbidden, check, self.makeRequest("GET"))
+
+    def testPOSTRequestAllowed(self):
+        check(self.makeRequest("POST"))
+
+
+def test_suite():
+    suite=TestSuite()
+    suite.addTest(makeSuite(PostOnlyTests))
+    return suite
+
