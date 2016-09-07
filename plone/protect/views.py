062e1d17adbbc1dd31152db0a75cf07e21cc33c1
Maurits van Rees <maurits@vanrees.org>
2016-09-07 17:00:13 +0200

    Only try the confirm view for urls that are in the portal.
    This applies PloneHotfix20160830.

--- plone/protect/views.py
+++ plone/protect/views.py
@@ -1,8 +1,16 @@
 from plone.protect.interfaces import IConfirmView
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
+from zExceptions import Forbidden
 from zope.interface import implementer
 
 
 @implementer(IConfirmView)
 class ConfirmView(BrowserView):
-    pass
+
+    def __call__(self):
+        urltool = getToolByName(self.context, 'portal_url')
+        original_url = getattr(self.request, 'original_url', '')
+        if not original_url or not urltool.isURLInPortal(original_url):
+            raise Forbidden('url not in portal')
+        return self.index()
