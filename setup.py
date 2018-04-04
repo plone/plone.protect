30d2cbf68d2de700b549acf8a35fc9d771f7d5b7
Maurits van Rees <maurits@vanrees.org>
2018-04-04 22:53:14 +0200

    Updated classifiers: Plone 4.3, 5.0, 5.1 and 5.2.
    [ci skip]

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Framework :: Zope2",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
