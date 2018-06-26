ef7416cb64cf263a36144120414f92e665bd8411
David Glick <david@glicksoftware.com>
2018-06-26 23:42:56 +0200

    Head, meet desk

--- plone/protect/__init__.py
+++ plone/protect/__init__.py
@@ -4,4 +4,3 @@ from plone.protect.authenticator import createToken
 from plone.protect.authenticator import CustomCheckAuthenticator
 from plone.protect.postonly import check as PostOnly
 from plone.protect.utils import protect
-from plone.protect import monkey
