a1bd3ef62992094ba693c8773906197d625e65ad
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:28:40 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='zope security CSRF',
     author='Plone Foundation',
