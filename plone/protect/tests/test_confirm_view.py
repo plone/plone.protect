71975c0239019b1fd4776c0606e4ec4add6f17f3
ale-rt <alessandro.pisa@gmail.com>
2025-06-04 12:18:19 +0200

    Do not allow the confirm-action view to be called without a valid authenticator

--- plone/protect/tests/test_confirm_view.py
+++ plone/protect/tests/test_confirm_view.py
@@ -1,3 +1,4 @@
+from plone.protect.authenticator import createToken
 from plone.protect.testing import PROTECT_FUNCTIONAL_TESTING
 from zExceptions import Forbidden
 from zope.component import getMultiAdapter
@@ -12,12 +13,26 @@ class TestAttackVector(unittest.TestCase):
         portal = self.layer["portal"]
         request = self.layer["request"]
         view = getMultiAdapter((portal, request), name="confirm-action")
-        request.form.update({"original_url": "foobar"})
+        request.form.update({"original_url": "foobar", "_authenticator": createToken()})
         self.assertTrue('value="foobar"' in view())
 
     def test_valid_url(self):
         portal = self.layer["portal"]
         request = self.layer["request"]
         view = getMultiAdapter((portal, request), name="confirm-action")
-        request.form.update({"original_url": "javascript:alert(1)"})
+        request.form.update(
+            {"original_url": "javascript:alert(1)", "_authenticator": createToken()}
+        )
         self.assertRaises(Forbidden, view)
+
+    def test_valid_authenticator(self):
+        portal = self.layer["portal"]
+        request = self.layer["request"]
+        view = getMultiAdapter((portal, request), name="confirm-action")
+        request.form.update(
+            {"original_url": portal.absolute_url(), "_authenticator": "foobar"}
+        )
+
+        with self.assertRaises(Forbidden) as cm:
+            view()
+        self.assertEqual(str(cm.exception), "Form authenticator is invalid.")
